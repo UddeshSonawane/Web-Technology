93a9551978e05729b52ae56854e919748a37b580
UddeshSonawane <47684657+UddeshSonawane@users.noreply.github.com>
2019-04-23 13:32:35 +0530

    Create compare.php

---
+++ PHP String Operations/compare.php
@@ -0,0 +1,26 @@
+<!Doctype html>
+<html>
+    <head><title>String Compare</title></head>
+    <body>
+        <?php
+            $st1=$_POST['st1'];
+            $st2=$_POST['st2'];
+            
+            echo "$st1\n$st2\n";
+           
+            echo strcmp($st1,$st2);  
+        ?>
+        <center>
+            <form name="myform" method="post">
+                <h1>Comparison of strings</h1>
+                <label>Enter String1</label>
+                <input type="text" name="st1"><br>
+                <label>Enter String2</label>
+                <input type="text" name="st2"><br>
+                <input type="submit" name="submit">
+            </form>        
+        </center>    
+    </body>
+</html>
+
+
