3485a6510f497b8c420831a3ad9c70ca62b4c13a
UddeshSonawane <47684657+UddeshSonawane@users.noreply.github.com>
2019-04-23 13:31:44 +0530

    Create index.php

---
+++ PHP String Operations/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>String Manipulation</title>
+    </head>
+    <body>
+    <center>
+        <h1><label>String Manipulation Program using PHP</label></h1>
+        <h3>Menu</h3>
+        <table>
+            <tr>
+                <td><a href="length.php">Find length of string</a></td>
+            </tr>
+            <tr>
+                <td><a href="compare.php">Compare strings</a></td>
+            </tr>
+            <tr>
+                <td><a href="strcase.php">Change case of String</a></td>
+            </tr>
+            <tr>
+                <td><a href="reverse.php">Reversing string</a></td>
+            </tr>
+            <tr>
+                <td><a href="concat.php">String concatinate</a></td>
+            </tr>
+            <tr>
+                <td><a href="replace.php">Relacing substring</a></td>
+            </tr>
+        </table>
+    </center>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
