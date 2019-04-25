4b641acf063df94320e88722298e271aa08b4d5b
UddeshSonawane <47684657+UddeshSonawane@users.noreply.github.com>
2019-04-25 22:11:07 +0530

    Create index.php

---
+++ php-mysql database operations/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Alumini Portal</title>
+    </head>
+    <body>
+    <center>
+        <h1>Alumini Data</h1>
+        <h2>Choices available</h2><br>
+        <p>
+            <label><a href="insert.php">Insert Data</a></label><br>
+            <label><a href="delete.php">Delete Data</a></label><br>
+            <label><a href="update.php">Update Data</a></label><br>
+            <label><a href="displayrecords.php"</a></label>
+        </p>
+    </center>
+        
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
